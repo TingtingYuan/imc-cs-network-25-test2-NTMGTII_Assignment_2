ffccb9a9c3b40b7d282350a16fbcf0b6dac83e0a
Tingting Yuan <45340148+TingtingYuan@users.noreply.github.com>
2025-10-18 00:03:11 +0800

    Update rpc_client.py

--- rpc_client.py
+++ rpc_client.py
@@ -1,10 +1,14 @@
 from rpc import RPCClient
 
-server = RPCClient('127.0.0.1', 8080)
+if __name__ == "__main__":
+    client = RPCClient('127.0.0.1', 8080)
 
-server.connect()
+    client.connect()
 
-print(server.add(5, 6))
-print(server.sub(5, 6))
+    print("add(5, 6) =", client.add(5, 6))
+    print("sub(5, 6) =", client.sub(5, 6))
+    print("fibonacci(10) =", client.fibonacci(10))
+    print("checksum([1, 2, 3, 4]) =", client.checksum([1, 2, 3, 4]))
 
-server.disconnect()
+    client.disconnect()
+
